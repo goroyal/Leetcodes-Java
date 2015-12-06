be465984a880e085e09343fabca780393292f2c6
freshdgq <dgq.cs.se@gmail.com>
2015-12-06 17:38:30 +0800

    refactor structure

---
+++ Array/src/RemoveElement.java
@@ -0,0 +1,26 @@
+/**
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * Created by dgq on 15/12/6.
+ */
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int p=0,q=0;
+		while (q < nums.length){
+			if (nums[q] == val){
+				q++;
+			}
+			if (q != nums.length && nums[q] != val){
+				nums[p] = nums[q];
+				p++;
+				q++;
+			}
+		}
+		return p;
+	}
+
+	public static void main(String[] args){
+		int[] nums = {3,3};
+		System.out.println(new RemoveElement().removeElement(nums,3));
+	}
+}
