468a5bf440fc9c07147cffc5c20c5472a3ace754
unknown <I320015@PVGN50865991A.apj.global.corp.sap>
2016-01-06 18:05:31 +0800

    add rotate array

--- Array/src/RemoveDuplicatesFromSortedArray.java
+++ Array/src/RemoveDuplicatesFromSortedArray.java
@@ -5,7 +5,7 @@
  * Given input array nums = [1,1,2],
  * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
  * It doesn't matter what you leave beyond the new length.
- * Created by dgq on 15/12/6.
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/
  */
 public class RemoveDuplicatesFromSortedArray {
 	public int removeDuplicates(int[] nums) {
