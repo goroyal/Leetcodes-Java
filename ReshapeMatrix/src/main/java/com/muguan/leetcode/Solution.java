b50470d86043abf67d11a085aa79d3b5cc1778e4
muguan <dgq.cs.se@gmail.com>
2017-05-24 00:42:12 +0800

    :star: create plus one and reshape matrix module.

---
+++ ReshapeMatrix/src/main/java/com/muguan/leetcode/Solution.java
@@ -0,0 +1,12 @@
+package com.muguan.leetcode;
+
+/**
+ * Created by dgq on 2017/5/24.
+ */
+public class Solution {
+	public int[][] matrixReshape(int[][] nums, int r, int c) {
+
+
+		return nums;
+	}
+}
---
+++ ReshapeMatrix/src/test/java/com/muguan/leetcode/SolutionTest.java
@@ -0,0 +1,13 @@
+package com.muguan.leetcode;
+
+import org.junit.Test;
+
+/**
+ * Created by dgq on 2017/5/24.
+ */
+public class SolutionTest {
+	@Test
+	public void testMatrixReshape(){
+
+	}
+}
