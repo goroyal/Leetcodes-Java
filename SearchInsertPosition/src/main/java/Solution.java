c2001cf64861d88e343adcbf7159d6d023f7fcaa
muguan <dgq.cs.se@gmail.com>
2017-08-27 23:20:39 +0800

    :star: complete best time to buy and sell stock module

---
+++ SearchInsertPosition/src/main/java/Solution.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dgq on 2017/8/27.
+ */
+public class Solution {
+	public int searchInsert(int[] nums, int target) {
+		return 0;
+	}
+}
---
+++ SearchInsertPosition/src/test/java/SolutionTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by dgq on 2017/8/27.
+ */
+public class SolutionTest {
+}
