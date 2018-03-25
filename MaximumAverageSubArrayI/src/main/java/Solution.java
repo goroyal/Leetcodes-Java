c766a0a7d2704e5f4695e214fbba6a416a68e767
muguan <dgq.cs.se@gmail.com>
2018-03-25 14:07:23 +0800

    complete max average sub array 1, space: O(1), time: O(n)
    update gradle wrapper, using bin zip instead of all

---
+++ MaximumAverageSubArrayI/src/main/java/Solution.java
@@ -0,0 +1,23 @@
+
+/**
+ * Created by dgq on 2017/10/3.
+ */
+public class Solution {
+	public double findMaxAverage(int[] nums, int k) {
+		if (k > nums.length) {
+			throw new RuntimeException("k should not over nums' length");
+		}
+		int sum = 0;
+		for (int i = 0; i < k; i++) {
+			sum += nums[i];
+		}
+		int max = sum;
+		for (int i = k; i < nums.length; i++) {
+			sum += nums[i] - nums[i - k];
+			if (sum > max) {
+				max = sum;
+			}
+		}
+		return ((double) max) / k;
+	}
+}
