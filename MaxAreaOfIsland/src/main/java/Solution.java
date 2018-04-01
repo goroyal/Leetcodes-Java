60276e2b912d4e023997d24a5105424766dbd007
muguan <dgq.cs.se@gmail.com>
2018-04-01 17:53:41 +0800

    Complete Max Area of Island

---
+++ MaxAreaOfIsland/src/main/java/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+  public int maxAreaOfIsland(int[][] grid) {
+    int rows = grid.length;
+    int columns = grid[0].length;
+
+    boolean[][] seen = new boolean[rows][columns];
+
+    int max = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        int value = area(grid, seen, i, j);
+        if (value > max) {
+          max = value;
+        }
+      }
+    }
+
+    return max;
+  }
+
+  private int area(int[][] grid, boolean[][] seen, int row, int column) {
+    if (row < 0 || column < 0 || row >= grid.length || column >= grid[0].length ||
+        seen[row][column] || grid[row][column] == 0) {
+      return 0;
+    }
+    seen[row][column] = true;
+    return 1 + area(grid, seen, row-1, column) + area(grid, seen, row, column-1) +
+              area(grid, seen, row+1, column) + area(grid, seen, row, column+1);
+  }
+}
