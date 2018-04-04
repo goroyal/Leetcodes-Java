ae3b30524e373a59f0e7033c171dbf73719ab40f
muguan <dgq.cs.se@gmail.com>
2018-04-04 21:14:36 +0800

    complete maximum sub array

---
+++ MaxSubarray/src/main/java/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+	public int maxSubArray(int[] nums) {
+		int maxResult = nums[0];
+		int maxCurrent = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (maxCurrent + nums[i] > nums[i]) {
+				maxCurrent += nums[i];
+			} else {
+				maxCurrent = nums[i];
+			}
+			if (maxCurrent > maxResult) {
+				maxResult = maxCurrent;
+			}
+		}
+		return maxResult;
+	}
+}
