4cfdd8a00839cadafb441e7e4bbb2d464e7f6e7b
muguan <dgq.cs.se@gmail.com>
2018-04-06 19:18:58 +0800

    complete can place flowers

---
+++ 605.CanPlaceFlowers/src/test/java/SolutionTest.java
@@ -0,0 +1,83 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SolutionTest {
+	private Solution solution;
+
+	@Before
+	public void setUp() {
+		solution = new Solution();
+	}
+
+	@Test
+	public void oneEmpty_CanPlaceOneFlower() {
+		int[] nums = {0};
+
+		Assert.assertTrue(solution.canPlaceFlowers(nums, 1));
+	}
+
+	@Test
+	public void twoEmpty_CannotPlaceTwoFlowers() {
+		int[] nums = {0, 0};
+
+		Assert.assertFalse(solution.canPlaceFlowers(nums, 2));
+	}
+
+	@Test
+	public void fourEmpty_CannotPlaceThreeFlowers() {
+		int[] nums = {0, 0, 0, 0};
+
+		Assert.assertFalse(solution.canPlaceFlowers(nums, 3));
+	}
+
+	@Test
+	public void oneEmptyBesideOneFlower_NoFlowerCanPlace() {
+		int[] nums = {1, 0};
+
+		Assert.assertFalse(solution.canPlaceFlowers(nums, 1));
+	}
+
+	@Test
+	public void emptyBesideEachFlower_NoFlowerCanPlace() {
+		int[] nums = {1, 0, 1, 0, 1};
+
+		Assert.assertFalse(solution.canPlaceFlowers(nums, 1));
+	}
+
+	@Test
+	public void twoEmptyAndOneEmpty_CanPlaceOneFlower() {
+		int[] nums = {0, 0, 1, 0, 1};
+
+		Assert.assertTrue(solution.canPlaceFlowers(nums, 1));
+	}
+
+	@Test
+	public void threeEmpty_CanPlaceOneFlower() {
+		int[] nums = {1, 0, 0, 0, 1};
+
+		Assert.assertTrue(solution.canPlaceFlowers(nums, 1));
+	}
+
+	@Test
+	public void threeEmpty_CannotPlaceTwoFlowers() {
+		int[] nums = {1, 0, 0, 0, 1};
+
+		Assert.assertFalse(solution.canPlaceFlowers(nums, 2));
+	}
+
+	@Test
+	public void threeEmptyAndTwoEmptyAtBorder_CanPlaceTwoFlowers() {
+		int[] nums = {1, 0, 0, 0, 1, 0, 0};
+
+		Assert.assertTrue(solution.canPlaceFlowers(nums, 2));
+	}
+
+	@Test
+	public void fourEmptyAtLeftBorderAndTwoEmptyAtRightBorder_CanPlaceThreeFlowers() {
+		int[] nums = {0, 0, 0, 0, 1, 0, 0};
+
+		Assert.assertTrue(solution.canPlaceFlowers(nums, 3));
+	}
+
+}
