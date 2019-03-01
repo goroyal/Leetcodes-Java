93ea564bf42ab9f8a22ec55c0b1180bc0911d16e
Goroyal Dai <dgq.cs.se@gmail.com>
2019-03-01 17:26:05 +0800

    Use greedy algorithm to solve non overlapping segment problem (#49)
    * update code writing for max subarray
    
    * Use greedy algorithm to solve non overlapping segment problem

--- 053.MaxSubarray/src/main/java/Solution.java
+++ 053.MaxSubarray/src/main/java/Solution.java
@@ -3,7 +3,7 @@ class Solution {
 		int maxResult = nums[0];
 		int maxCurrent = nums[0];
 		for (int i = 1; i < nums.length; i++) {
-			if (maxCurrent + nums[i] > nums[i]) {
+			if (maxCurrent > 0) {
 				maxCurrent += nums[i];
 			} else {
 				maxCurrent = nums[i];
