0e08001d83640c8bcf9eb212187c3809ad165857
Goroyal <dgq.cs.se@gmail.com>
2019-08-22 00:21:10 +0800

    Complete problem 448 and 442

--- 448.FindDisappearedNumbers/src/main/java/Solution.java
+++ 448.FindDisappearedNumbers/src/main/java/Solution.java
@@ -3,21 +3,19 @@ import java.util.List;
 
 class Solution {
   public List<Integer> findDisappearedNumbers(int[] nums) {
-    List<Integer> result = new ArrayList<>(nums.length);
+    List<Integer> results = new ArrayList<>();
 
     for (int i = 0; i < nums.length; i++) {
-	    // 第i个数的值所表示的位置，将它设为负的。
-	    int position = Math.abs(nums[i]) - 1;
-	    if (nums[position] > 0) {
-		    nums[position] = -nums[position];
-      }
+      int indexToChange = Math.abs(nums[i])  - 1;
+      int value = nums[indexToChange];
+      nums[indexToChange] = value > 0 ? -value : value;
     }
-	  // 到最后，如果缺少哪些数，那么这些数所指向的位置的值就没有更改过
+
     for (int i = 0; i < nums.length; i++) {
-	    if (nums[i] > 0) {
-		    result.add(i + 1);
+      if (nums[i] > 0) {
+        results.add(i + 1);
       }
     }
-    return result;
+    return results;
   }
 }
