9863563b05524eead4381e0a383bff1e92fa669d
Goroyal Dai <dgq.cs.se@gmail.com>
2019-09-04 21:10:04 +0800

    Complete minimum difference in binary search tree (#58)

---
+++ 530.MinimumAbsoluteDifferenceInBST/src/main/java/Solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+  public int getMinimumDifference(TreeNode root) {
+    if (root == null) {
+      return Integer.MAX_VALUE;
+    }
+    TreeNode left = root.left != null ? getLargestNode(root.left) : null;
+    TreeNode right = root.right != null ? getSmallestNode(root.right) : null;
+    int current;
+    if (left != null && right != null) {
+      current = Math.min(root.val - left.val, right.val - root.val);
+    } else if (left == null && right != null){
+      current = right.val - root.val;
+    } else if (left != null && right == null) {
+      current = root.val - left.val;
+    } else {
+      return Integer.MAX_VALUE;
+    }
+    int leftResult = getMinimumDifference(root.left);
+    int rightResult = getMinimumDifference(root.right);
+    return Math.min(Math.min(leftResult, rightResult), current);
+  }
+
+  private TreeNode getLargestNode(TreeNode root) {
+    TreeNode p = root;
+    while (p.right != null) {
+      p = p.right;
+    }
+    return p;
+  }
+
+  private TreeNode getSmallestNode(TreeNode root) {
+    TreeNode p = root;
+    while (p.left != null) {
+      p = p.left;
+    }
+    return p;
+  }
+}
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) { val = x; }
+}
