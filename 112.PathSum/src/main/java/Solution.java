18bcf05cab9222595b40324d93f13e7cc5a556fa
Goroyal <dgq.cs.se@gmail.com>
2020-05-24 21:18:12 +0800

    Add tree solutions for 637 and 112

---
+++ 112.PathSum/src/main/java/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+  public boolean hasPathSum(TreeNode root, int sum) {
+    if (root == null) {
+      return false;
+    }
+
+    return hasPathSum(root, 0, sum);
+  }
+
+  private boolean hasPathSum(TreeNode node, int traversaledSum, int sum) {
+    if (node.left == null && node.right == null) {
+      return node.val + traversaledSum == sum;
+    }
+    boolean left = node.left != null && hasPathSum(node.left, node.val + traversaledSum, sum);
+    boolean right = node.right != null && hasPathSum(node.right, node.val + traversaledSum, sum);
+    return left || right;
+  }
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+}
