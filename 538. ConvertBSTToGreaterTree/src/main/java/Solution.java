84717391998fc50da0e6e18c8bd85b6ace33c7fb
Goroyal Dai <dgq.cs.se@gmail.com>
2020-06-01 16:48:04 +0800

    Tree solution on June 1, 2020 (#61)

---
+++ 538. ConvertBSTToGreaterTree/src/main/java/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+  private int sum = 0;
+
+  public TreeNode convertBST(TreeNode root) {
+    if (root != null) {
+      convertBST(root.right);
+      sum += root.val;
+      root.val = sum;
+      convertBST(root.left);
+    }
+    return root;
+  }
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+}
